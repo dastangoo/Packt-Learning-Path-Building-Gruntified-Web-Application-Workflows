17d9e8bb46d77db11497b007b51d440703ed67f5
dastangoo <hamed.dastangoo@gmail.com>
2017-06-03 19:32:17 +0430

    Uglify and CSSMin

--- gruntfile.js
+++ gruntfile.js
@@ -27,12 +27,35 @@ module.exports = function (grunt) {
         options: {
           sourceMap: true
         }
+      },
+      uglify: {
+        dist: {
+          files: {
+            'dist/js/package.min.js': 'dist/js/**/*.js'
+          }
+        },
+        options: {
+          sourceMap: true,
+          sourceManIn: 'dist/js/package.js.map'
+        }
+      },
+      cssmin: {
+        dist: {
+          files: {
+            'dist/css/styles.min.css': 'dist/css/**/*.css'
+          }
+        },
+        options: {
+          sourceMap: true
+        }
       }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-sass');
-  grunt.registerTask('default', ['jshint', 'coffee', 'sass']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['jshint', 'coffee', 'sass', 'uglify', 'cssmin']);
 
 }
